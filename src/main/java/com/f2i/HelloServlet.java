5d97dfd6d0acb2c84248b866b2caaf9aa298eb08
rijaandrianivo <56109507+rijaandrianivo@users.noreply.github.com>
2019-10-03 12:24:58 +0200

    update servlet

--- src/main/java/com/f2i/HelloServlet.java
+++ src/main/java/com/f2i/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		response.getWriter().println("Hello Servlets : ");
+		response.getWriter().println("Hello Servlet : ");
 	}
 
 	/**
